c1f91124248fd153aa713bdd24b3e5cf97afae9d
NalenSrin123 <161912473+NalenSrin123@users.noreply.github.com>
2025-01-19 10:25:23 +0700

    Header, Section,Footer

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+function App(){
+  return(
+    <>
+      <header>
+        <div className="header">
+          <img src="https://news.sabay.com.kh/img/logo.png" alt="" />
+        </div>
+      </header>
+      <nav>
+        <div className="menu">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">កម្សាន្ត</a></li>
+            <li><a href="#">បច្ចេកវិទ្យា</a></li>
+            <li><a href="#">កីឡា</a></li>
+            <li><a href="#">Auto Talk</a></li>
+          </ul>
+        </div>
+      </nav>
+      <section>
+    <h1>Main Content</h1>
+      </section>
+      <footer>
+    <h1>Footer</h1>
+      </footer>
+    </>
+  );
+}
+export default App;
